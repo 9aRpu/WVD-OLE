94763b94000db49bb14eb18804d62b1e402405a5
9aRpu <77620790+9aRpu@users.noreply.github.com>
2021-03-31 14:49:13 +0200

    Update 1_installOneDrive.ps1

--- 1_installOneDrive.ps1
+++ 1_installOneDrive.ps1
@@ -8,11 +8,11 @@ $destination = "$LocalPath\OneDriveSetup.exe"
 Invoke-WebRequest $source -OutFile $destination
 
 # Start the installation when download is finished
-Start-Process -FilePath "$LocalPath\OneDriveSetup.exe" -ArgumentList "/uninstall" -Wait
+Start-Process -FilePath "$LocalPath\OneDriveSetup.exe" -ArgumentList "/uninstall /silent" -Wait
 
 cmd.exe /c 'REG ADD "HKLM\Software\Microsoft\OneDrive" /v "AllUsersInstall" /t REG_DWORD /d 1 /reg:64'
 
-Start-Process -FilePath "$LocalPath\OneDriveSetup.exe" -ArgumentList "/allusers" -Wait
+Start-Process -FilePath "$LocalPath\OneDriveSetup.exe" -ArgumentList "/allusers /silent" -Wait
 
 cmd.exe /c 'REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f'
 
